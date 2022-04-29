3f6c33f788bc086cfdf8f9fcce34b602e34129ba
odinfs <72218018+odinfs@users.noreply.github.com>
2022-04-29 20:10:37 +0200

    Making FrontPage

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,46 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDom.render(<h1>Hello, Worldy!</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Welcome to my awesome site, you bastard!</h1>
+      <ul>
+        <li>
+          <Link to={"Login"}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <from>
+      <h1>Login</h1>
+      <div>
+        Username <input />
+      </div>
+      <div>
+        Password <input type={"password"} />
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </from>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDom.render(<Application />, document.getElementById("app"));
